536e592f88d8a7ddb6937d42a3250c92ddd7dff5
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-12-01 00:04:53 +0600

    table created

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 // const express = require("express");
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
+import Path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: Path.join(process.cwd(), ".env") });
 const app = express();
 const port = 5000;
 
@@ -9,9 +13,38 @@ app.use(express.json());
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// PostgreSQL connection pool
 const pool = new Pool({
-  connectionString: `postgresql://neondb_owner:npg_WEX7tSBYhri5@ep-proud-cell-a48oj3cx-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+  connectionString: `${process.env.CONNECTION_STRING}`,
 });
+
+const initDb = async () => {
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL,
+      age INT,
+      phone VARCHAR(15),
+      address TEXT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+
+    )`);
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS todos (
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id) ON DELETE CASCADE,
+      title VARCHAR(255) NOT NULL,
+      description TEXT,
+      completed BOOLEAN DEFAULT FALSE,
+      due_date DATE,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )`);
+};
+
+initDb();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
