221b1b317da21352cb414b60728a2410ea4902c4
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-11-30 22:24:41 +0600

    postgray connected

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
 // const express = require("express");
 import express, { Request, Response } from "express";
+import { Pool } from "pg";
 const app = express();
 const port = 5000;
 
+// parse application/json
+app.use(express.json());
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString: `postgresql://neondb_owner:npg_WEX7tSBYhri5@ep-proud-cell-a48oj3cx-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+app.post("/data", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(200).json({
+    success: true,
+    message: "Data received successfully",
+  });
 });
 
 app.listen(port, () => {
